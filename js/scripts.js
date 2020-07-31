54387cfc38f83f5c0298b34c99dfb142e256e5cb
DileepNalla <dileepnalla6@gmail.com>
2020-08-01 01:04:34 +0530

    Created new PX tag

--- js/scripts.js
+++ js/scripts.js
@@ -153,6 +153,6 @@ function deleteAllCookies() {
         var cookie = cookies[i];
         var eqPos = cookie.indexOf("=");
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
