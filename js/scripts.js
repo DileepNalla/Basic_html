e9c00f41d8f22707521771b28b1e13e0a7008d68
Dileep Nalla <dileepnalla6@gmail.com>
2020-12-21 13:38:44 +0530

    Pendo Installation

--- js/scripts.js
+++ js/scripts.js
@@ -41,6 +41,18 @@ window['_fs_namespace'] = 'FS';
 })(window,document,window['_fs_namespace'],'script','user');
 
 
+/*Pendo.io*/
+(function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
+    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+})('e198fa5a-4a04-4d7e-77cf-11a684ab0378');
+
+
+
+
 
 /*Script for Login Validation */
 
@@ -109,6 +121,36 @@ function allowlogin(usermail) {
                      // flat custom attributes
                 });
 
+           
+            // Call this whenever information about your visitors becomes available
+            // Please use Strings, Numbers, or Bools for value types.
+            pendo.initialize({
+                visitor: {
+                    id: id ,  // Required if user is logged in
+                     email: a       // Recommended if using Pendo Feedback, or NPS Email
+                    // full_name:    // Recommended if using Pendo Feedback
+                    // role:         // Optional
+
+                    // You can add any additional visitor level key-values here,
+                    // as long as it's not one of the above reserved names.
+                },
+
+                account: {
+                    id:    myaccount.a1 // Highly recommended
+                    // name:         // Optional
+                    // is_paying:    // Recommended if using Pendo Feedback
+                    // monthly_value:// Recommended if using Pendo Feedback
+                    // planLevel:    // Optional
+                    // planPrice:    // Optional
+                    // creationDate: // Optional
+
+                    // You can add any additional account level key-values here,
+                    // as long as it's not one of the above reserved names.
+                }
+            });
+
+
+// Full story
                 // This is an example script - don't forget to change it!
 FS.identify(id, {
     displayName: a,
