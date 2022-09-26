4619bf41ed74a4a56298eab3dffc51247ae74189
Dileep Nalla <dileepnalla6@gmail.com>
2022-09-26 14:54:22 +0530

    Updating the Account Info

--- js/scripts.js
+++ js/scripts.js
@@ -289,7 +289,7 @@ function allowlogin(usermail) {
 
 
     if (a != "") {
-        if (a == "dileepnalla29@gmail.com" || a == "y09me034@gmail.com" || a == "dileepnalla6@gmail.com" || a == "dnalla@gainsight.com" || a == "demouser@gmail.com") {
+        if (a == "dileepnalla29@gmail.com" || a == "y09me034@gmail.com" || a == "dileepnalla6@gmail.com" || a == "dnalla@gainsight.com" || a == "demouser@gmail.com"|| a == "nareshkumar564@gmail.com") {
             alert("valid User");
             b = a.substr(8, 5);
             var id = b;
